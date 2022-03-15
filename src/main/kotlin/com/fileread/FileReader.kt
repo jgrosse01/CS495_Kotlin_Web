fa78a3bb05e274a3deb0be94dc663466ceaf4e82
Jake Grosse <jgrosse@carroll.edu>
2022-03-15 10:05:19 -0600

    added last comments (BEFORE DEADLINE)

--- src/main/kotlin/com/fileread/FileReader.kt
+++ src/main/kotlin/com/fileread/FileReader.kt
@@ -3,11 +3,19 @@ package com.fileread
 import java.io.File
 import java.nio.file.Paths
 
+/**
+ * Function that reads a file and returns it as a string.
+ */
 fun readFile(fileName: String): String {
+    // open file from root of project, formatted as absolute path
     val file = File(Paths.get("src/main/resources/$fileName").toAbsolutePath().toString())
+    // empty string to add to
     var retString = ""
+    // read lines
     file.forEachLine {
+        // add line to string
         retString += it
     }
+    // return string
     return(retString)
 }
